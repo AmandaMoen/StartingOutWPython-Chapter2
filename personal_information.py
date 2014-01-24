678628eb3d6a5f7912787dd5bb3d10e5f488320b
Amanda Moen <amandalmoen@yahoo.com>
2014-01-23 19:57:47 -0800

    Add personal_information.py

---
+++ personal_information.py
@@ -0,0 +1,18 @@
+# Programming Exercise 2.1 Personal Information
+# April 12th, 2010
+# CS110,
+# Amanda L. Moen
+# 1. Personal Information
+# Write a program that displays the following information:
+#    Your name
+#    Your address, city, state, ZIP
+#    Your telephone number
+#    Your college major
+
+# This program display's a person's name, address, telephone number,
+# and college major
+
+print 'Amanda Moen'
+print '11826 Lake Stevens, WA 98258'
+print '2191'
+print 'Computer Information Systems'
